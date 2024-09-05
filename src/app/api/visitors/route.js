02602f44d21e47ecd0f12817367865a7badfa193
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-05 20:02:55 +0200

    fix prisma

--- src/app/api/visitors/route.js
+++ src/app/api/visitors/route.js
@@ -1,19 +1,25 @@
+'use server'
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function GET() {
+  console.log("Starting GET request");
+
   try {
     const visitor = await prisma.visitor.findFirst();
+    console.log("Visitor data retrieved:", visitor);
 
     if (!visitor) {
       const newVisitor = await prisma.visitor.create({ data: { visitCount: 1 } });
+      console.log("New visitor created:", newVisitor);
       return new Response(JSON.stringify({ count: newVisitor.visitCount }), { status: 200 });
     } else {
       const updatedVisitor = await prisma.visitor.update({
         where: { id: visitor.id },
         data: { visitCount: visitor.visitCount + 1 },
       });
+      console.log("Visitor count updated:", updatedVisitor);
       return new Response(JSON.stringify({ count: updatedVisitor.visitCount }), { status: 200 });
     }
   } catch (error) {
