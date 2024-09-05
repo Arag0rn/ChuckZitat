ae83c70a0655c9047d5064862b83c686a92c92fc
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-05 18:55:34 +0200

    fixed routes

--- src/app/api/joke/jokeService.js
+++ src/app/api/joke/jokeService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export async function GET() {
+export async function fetchJoke() {
   try {
     const response = await axios.get('https://api.chucknorris.io/jokes/random?category=dev');
     const quote = response.data.value;
-    return new Response(JSON.stringify({ quote }), { status: 200 });
+    return { quote };
   } catch (error) {
     console.error('Error fetching joke:', error);
-    return new Response(JSON.stringify({ error: 'Failed to fetch joke' }), { status: 500 });
+    throw new Error('Failed to fetch joke');
   }
 }
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,24 +1,36 @@
 'use client'
 import { useEffect, useState } from 'react';
+import { fetchJoke } from '../api/joke/jokeService';
 
 export const Main =() =>{
   const [joke, setJoke] = useState('');
   const [visitCount, setVisitCount] = useState(0);
 
   useEffect(() => {
-    const fetchJoke = async () => {
-      const res = await fetch('/api/joke');
-      const data = await res.json();
-      setJoke(data.quote);
-    };
+    async function getJokeForComponent() {
+      try {
+        const result = await fetchJoke();
+        setJoke(result.quote);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.error('An unknown error occurred');
+        }
+      }
+    }
 
     const fetchVisitorCount = async () => {
-      const res = await fetch('/api/visitors');
-      const data = await res.json();
-      setVisitCount(data.count);
+      try {
+        const response = await fetch('/api/visitors'); 
+        const data = await response.json();
+        setVisitCount(data.count); 
+      } catch (error) {
+        console.error('Error fetching visitor count:', error);
+      }
     };
 
-    fetchJoke();
+    getJokeForComponent();
     fetchVisitorCount();
   }, []);
 
